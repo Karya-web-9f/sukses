4988693fa24412bf657518208ac736fb421844b9
Karya-web-9f <fotoijasahkelas9@gmail.com>
2024-11-18 13:57:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    window.location.href = "utama.html"; // Mengarahkan ke main.html setelah 10 detik
+}, 7000); // 5000 ms = 10 detik
+
+// Menyembunyikan teks setelah 7 detik
+ setTimeout(function() {
+    document.getElementById("typingText").style.display = "none";
+}, 7000); // 7000ms = 7 detik
+};
